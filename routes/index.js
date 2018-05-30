62a52a4fdc91960891b9dc08f7800f769f04a170
joe429273392 <a429273392@qq.com>
2018-05-30 11:35:35 +0800

    complete login

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/login', function(req, res, next) {
-  console.log(req);
-  res.send({});
+  var rs = [];
+  var result = {
+    status: 0,
+    info: "用户名或密码错误"
+  };
+  fs.readFile('./public/data/user.json', function(err, data) {
+    if(err){
+      result = {
+        status: 0,
+        info: err
+      };
+    }
+    rs = JSON.parse(data.toString());
+
+    var username = req.body.username || "";
+    var password = req.body.password || "";
+  
+    for(let user of rs){
+      if(user.username === username && user.password === password){
+        result = {
+          status: 1,
+          info: "登录成功"
+        };
+      }
+    }
+    
+    res.send(result);
+  })
+
+  
 })
 
 module.exports = router;
